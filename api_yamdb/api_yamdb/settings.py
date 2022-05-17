70761a24a3d652850f7ceb1e9c7f1a2803c1f93c
Виталий Насретдинов <nasretdinovs@yandex.ru>
2022-05-17 19:05:00 +0300

    All bad

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -11,6 +11,9 @@ SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
 DEBUG = False
 
 ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    '[::1]',
     '51.250.108.40',
     'vitalca.hopto.org',
 ]
